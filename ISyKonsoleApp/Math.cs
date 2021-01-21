d2338036fba29d98e213eaff5f3811082cac187b
kbuhlert <kbuhlert@gmail.com>
2021-01-21 09:47:28 +0100

    Projektdateien hinzufügen.

---
+++ ISyKonsoleApp/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISyKonsoleApp
+{
+    class Math
+    {
+        public int Add(int zahl1, int zahl2)
+        {
+            return zahl1 + zahl2;
+        }
+
+        public int Subtr(int zahl1, int zahl2)
+        {
+            return zahl1 - zahl2;
+        }
+    }
+}
---
+++ ISyKonsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISyKonsoleApp
+{
+    class Program
+    {
+        //hier wird neuer Datentyp "ZeigerAufAddMethod"e (Delegate) nicht als Klasse sonder als Delegate gespeichert. kompiler erstellt dann neuen Datentyp
+        delegate int ZeigerAufAddMethode(int z1, int z2);
+
+        static void Main(string[] args)
+        {
+            Math m1 = new Math();
+            int ergebnis = m1.Add(12, 5);
+
+            //delegate - Variable (vom Typ dieser Methoden Adresse, hier: Var die int zurückliefer und 2 int als Übergabeparameter) die Methoden-Adressen speichert
+            //-> Datentyp variable = m1.Add;        Variable die Methodenadresse speichert, aber die Methode noch nicht aufruft
+            ZeigerAufAddMethode z1 = m1.Add;    //= m1.Add()
+
+            int ergebnis2 = z1(12, 5);
+
+            //delegate jetzt auf andere Methode richten
+            z1 = m1.Subtr;
+            int ergebnis3 = z1(10, 2);
+        }
+    }
+}
